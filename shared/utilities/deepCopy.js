7dac9f8e2ce6770699f0e8813d6bd39cf40b59e1
0xstarcat <xxStarcat@gmail.com>
2019-10-30 11:35:05 -0400

    extend deep_copy with customTemplate behavior

--- shared/utilities/deepCopy.js
+++ shared/utilities/deepCopy.js
@@ -2,9 +2,13 @@ export const getEntry = async (space, id) => {
   return await space.getEntry(id);
 };
 
+const getEntrySys = entry => {
+  return entry.sys || entry.getSys()
+}
+
 
 export const cloneEntry = async (space, entry, name) => {
-  const entrySys = entry.sys || entry.getSys();
+  const entrySys = getEntrySys(entry);
   const contentType = entrySys.contentType.sys.id;
   const clonedEntry = await space.createEntry(contentType, {
     fields: await constructFields(space, entry, contentType, name)
@@ -16,31 +20,31 @@ const constructFields = async (space, entry, contentType, name) => {
   const fields = {};
   let multiReferenceCount = {};
   for (let [key, value] of Object.entries(entry.fields)) {
-    let parsedValue =
-      key === 'name' || key === 'internalName'
+    let parsedField =
+      key === 'name'
         ? name
         : value['en-US'] || value._fieldLocales['en-US']._value;
 
     if (
-      parsedValue &&
-      parsedValue.sys &&
-      parsedValue.sys.type === 'Link' &&
-      parsedValue.sys.linkType === 'Entry'
+      parsedField &&
+      parsedField.sys &&
+      parsedField.sys.type === 'Link' &&
+      parsedField.sys.linkType === 'Entry'
     ) {
       // Single reference
-      const linkEntry = await getEntry(space, parsedValue.sys.id);
+      const linkEntry = await getEntry(space, parsedField.sys.id);
       const linkEntryContentType = linkEntry.sys.contentType.sys.id;
       const clonedEntry = await cloneEntry(space, linkEntry, `${name} ${linkEntryContentType}`);
       fields[key] = {
         ['en-US']: constructLink(clonedEntry)
       };
     } else if (
-      (value.type === 'Array' && value.items && value.items.type === 'Link') ||
-      Array.isArray(value['en-US'])
+      !getEntrySys(entry).contentType.sys.id === 'customTemplate' && ((value.type === 'Array' && value.items && value.items.type === 'Link') ||
+      Array.isArray(value['en-US']))
     ) {
-      // Multi Reference
+      // Multi Reference, not customTemplate
       const references = await Promise.all(
-        parsedValue.map(async link => {
+        parsedField.map(async link => {
           const linkEntry = await getEntry(space, link.sys.id);
           const linkEntryContentType = linkEntry.sys.contentType.sys.id;
           multiReferenceCount[linkEntryContentType] = multiReferenceCount[linkEntryContentType]
@@ -59,12 +63,48 @@ const constructFields = async (space, entry, contentType, name) => {
       fields[key] = {
         ['en-US']: references
       };
-    } else {
+    } else if (!(getEntrySys(entry).contentType.sys.id === 'customTemplate' && key === 'internalMapping')) {
       // Assets, all other fields
       fields[key] = {
-        ['en-US']: parsedValue
+        ['en-US']: parsedField
       };
     }
+  }
+
+  // Handle customTemplate separately
+
+  if (getEntrySys(entry).contentType.sys.id === 'customTemplate') {
+    const entries = entry.fields.entries['en-US']
+    let internalMapping = entry.fields.internalMapping['en-US']
+    internalMapping = internalMapping ? JSON.parse(internalMapping) : internalMapping
+
+    let newInternalMapping = {}
+    let newEntries = []
+    await Promise.all(await Object.keys(internalMapping).map(async key => {
+      const entryId = entries.find(e => getEntrySys(e).id === internalMapping[key]).sys.id
+
+      if (entryId) {
+        const fetchedEntry = await getEntry(space, entryId)
+        const entryContentType = getEntrySys(entry).contentType.sys.id;
+
+        const clonedEntry = await cloneEntry(
+          space,
+          fetchedEntry,
+          `${name} ${entryContentType} ${key}`
+        );
+
+        newInternalMapping[key] = getEntrySys(clonedEntry).id
+        newEntries.push(constructLink(clonedEntry))
+      }
+    }))
+
+    fields['internalMapping'] = {
+      ['en-US']: JSON.stringify(newInternalMapping)
+    };
+
+    fields['entries'] = {
+      ['en-US']: newEntries
+    };
   }
 
   return fields;
