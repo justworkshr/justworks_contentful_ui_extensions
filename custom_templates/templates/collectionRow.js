22b17a2f225af3bab67f16a8aac3d3ee7a025ed7
0xstarcat <xxStarcat@gmail.com>
2019-12-04 18:07:27 -0500

    add file

---
+++ custom_templates/templates/collectionRow.js
@@ -0,0 +1,20 @@
+import { constructRole, allowAsset, fieldObject } from '../utils';
+import * as c from '../constants';
+
+export const logoItem = {
+  [c.LOGO_ITEM]: {
+    meta: {
+      hidden: true,
+      description: 'copy pending',
+      imageUrl: ""
+    },
+    fieldRoles: {
+      title: constructRole({
+        field: fieldObject({type: c.FIELD_TYPE_TEXT, defaultClasses: 'text-center text-black text-bold'}),
+        required: false,
+        description: 'Text beneath the logo.'
+      }),
+      items: {}
+    }
+  }
+}
