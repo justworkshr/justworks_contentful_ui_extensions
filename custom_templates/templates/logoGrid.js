79e19cba7019a5cd611d70e283a975b5a085bc9d
0xstarcat <xxStarcat@gmail.com>
2019-12-12 21:41:31 -0500

    update deep_copy

--- custom_templates/templates/logoGrid.js
+++ custom_templates/templates/logoGrid.js
@@ -1,28 +1,45 @@
-import { constructRole, allowAsset, fieldObject, allowMultipleReferences } from '../utils';
-import * as c from '../constants';
+import {
+  constructRole,
+  allowAsset,
+  fieldObject,
+  allowMultipleReferences
+} from "../utils";
+import * as c from "../constants";
 
 export const logoGrid = {
   [c.LOGO_GRID]: {
     meta: {
       hidden: true,
-      description: 'copy pending',
+      description: "copy pending",
       imageUrl: ""
     },
     fieldRoles: {
       title: constructRole({
-        description: 'Template title.',
+        description: "Template title.",
         required: false,
-        field: fieldObject({type: c.FIELD_TYPE_MARKDOWN}),
-        defaultClasses: 'text-center text-black'
+        field: fieldObject({ type: c.FIELD_TYPE_MARKDOWN }),
+        defaultClasses: "text-center text-black"
       }),
       items: constructRole({
-        description: 'Grid items.',
+        description: "Grid items.",
         required: false,
-        allowedCustomTemplates: [c.LOGO_ITEM],
-        ...allowAsset({type: c.ASSET_TYPE_IMAGE, subType: c.ASSET_SUBTYPE_LOGO, allowFormatting: false, maxWidth: '200', defaultClasses: 'icon-small icon-center'}),
-        ...allowMultipleReferences({allow: true, styleType: c.MULTI_REFERENCE_STYLE_FLEX, allowStyle:true,  contentTypes: ['customTemplate']}),
-        defaultClasses: 'flex-row flex-align-start flex-justify-center flex-items-per-1 small-flex-items-per-2 large-flex-items-per-4',
+        // allowedCustomTemplates: [c.LOGO_ITEM],
+        ...allowAsset({
+          type: c.ASSET_TYPE_IMAGE,
+          subType: c.ASSET_SUBTYPE_LOGO,
+          allowFormatting: false,
+          maxWidth: "200",
+          defaultClasses: "icon-small icon-center"
+        }),
+        ...allowMultipleReferences({
+          allow: true,
+          styleType: c.MULTI_REFERENCE_STYLE_FLEX,
+          allowStyle: true,
+          contentTypes: ["customTemplate"]
+        }),
+        defaultClasses:
+          "flex-row flex-align-start flex-justify-center flex-items-per-1 small-flex-items-per-2 large-flex-items-per-4"
       })
     }
   }
-}
+};
--- shared/utilities/deepCopy.js
+++ shared/utilities/deepCopy.js
@@ -1,3 +1,5 @@
+import * as c from "../../custom_templates/constants";
+
 export const getEntry = async (space, id) => {
   return await space.getEntry(id);
 };
@@ -28,20 +30,26 @@ const fetchMultiReferences = async (space, fieldValue, name) => {
   const multiReferenceCount = {};
   const references = await Promise.all(
     fieldValue.map(async link => {
-      const linkEntry = await getEntry(space, link.sys.id);
-      const linkEntryContentType = linkEntry.sys.contentType.sys.id;
-      multiReferenceCount[linkEntryContentType] = multiReferenceCount[
-        linkEntryContentType
-      ]
-        ? (multiReferenceCount[linkEntryContentType] += 1)
-        : 1;
-      const clonedEntry = await cloneEntry(
-        space,
-        linkEntry,
-        `${name} ${linkEntryContentType} ${multiReferenceCount[linkEntryContentType]}`
-      );
+      if ((link.sys || {}).linkType === "Asset") {
+        // Do not clone Asset
+        return constructLink(link);
+      } else {
+        // clone entry and add count name
 
-      return constructLink(clonedEntry);
+        const linkEntry = await getEntry(space, link.sys.id);
+        const linkEntryContentType = linkEntry.sys.contentType.sys.id;
+        multiReferenceCount[linkEntryContentType] = multiReferenceCount[
+          linkEntryContentType
+        ]
+          ? (multiReferenceCount[linkEntryContentType] += 1)
+          : 1;
+        const clonedEntry = await cloneEntry(
+          space,
+          linkEntry,
+          `${name} ${linkEntryContentType} ${multiReferenceCount[linkEntryContentType]}`
+        );
+        return constructLink(clonedEntry);
+      }
     })
   );
 
@@ -133,17 +141,16 @@ const constructCustomTemplateFields = async (
         ["en-US"]: constructLink(clonedEntry)
       };
     } else if (isMultiReference(value)) {
+      // Multi assets, multi references including for customTemplate
       if (key === "entries") continue;
-      // Multi Reference entry, custom Template
       const references = await fetchMultiReferences(space, parsedField, name);
 
       fields[key] = {
         ["en-US"]: references
       };
-      // TODO - add isSingleAsset
     } else {
       if (key === "internalMapping") continue;
-      // Assets, all other fields except customTemplate's internalMapping field
+      // Single assets, all other fields except customTemplate's internalMapping field
       fields[key] = {
         ["en-US"]: parsedField
       };
@@ -153,45 +160,103 @@ const constructCustomTemplateFields = async (
   // Handle customTemplate separately
 
   if (getEntrySys(entry).contentType.sys.id === "customTemplate") {
-    const entries = entry.fields.entries["en-US"];
+    const entries = !!entry.fields.entries
+      ? entry.fields.entries["en-US"]
+      : undefined;
+    const assets = !!entry.fields.assets
+      ? entry.fields.assets["en-US"]
+      : undefined; // Do not clone assets
     let internalMapping = entry.fields.internalMapping["en-US"];
     internalMapping = internalMapping
       ? JSON.parse(internalMapping)
       : internalMapping;
 
-    let newInternalMapping = {};
     let newEntries = [];
-    await Promise.all(
-      await Object.keys(internalMapping).map(async key => {
-        const entryId = entries.find(
-          e => getEntrySys(e).id === internalMapping[key]
-        ).sys.id;
 
-        if (entryId) {
-          const fetchedEntry = await getEntry(space, entryId);
-          const entryContentType = getEntrySys(entry).contentType.sys.id;
+    if (internalMapping.fieldRoles) {
+      await Promise.all(
+        await Object.keys(internalMapping.fieldRoles).map(async key => {
+          if (
+            internalMapping.fieldRoles[key].type ===
+            c.FIELD_TYPE_MULTI_REFERENCE
+          ) {
+            let values = new Array(
+              internalMapping.fieldRoles[key].value.length
+            ); // pre-construct blank array so async functions can populate in order by index
+            await Promise.all(
+              await internalMapping.fieldRoles[key].value.map(
+                async (mapping, index) => {
+                  if (mapping.type === c.FIELD_TYPE_ENTRY) {
+                    // clone asset, attached ID to mapping object value
+                    const entry = entries.find(
+                      e => getEntrySys(e).id === mapping.value
+                    );
+                    const clonedEntry = await getClonedEntry(
+                      space,
+                      entry.sys.id,
+                      createName(name, key)
+                    );
+                    values[index] = { ...mapping, value: clonedEntry.sys.id };
+                    newEntries.push(constructLink(clonedEntry));
+                  } else {
+                    // do not clone assets or fields
+                    values[index] = mapping;
+                  }
+                }
+              )
+            );
 
-          const clonedEntry = await cloneEntry(
-            space,
-            fetchedEntry,
-            `${name} ${entryContentType} ${key}`
-          );
+            internalMapping.fieldRoles[key].value = values;
+          } else if (
+            internalMapping.fieldRoles[key].type === c.FIELD_TYPE_ENTRY
+          ) {
+            const entry = entries.find(
+              e => getEntrySys(e).id === internalMapping.fieldRoles[key]
+            );
 
-          newInternalMapping[key] = getEntrySys(clonedEntry).id;
-          newEntries.push(constructLink(clonedEntry));
-        }
-      })
-    );
+            if (entry) {
+              const clonedEntry = await getClonedEntry(
+                space,
+                entry.sys.id,
+                createName(name, key)
+              );
+
+              internalMapping.fieldRoles[key] = {
+                ...internalMapping.fieldRoles[key],
+                value: getEntrySys(clonedEntry).id
+              };
+              newEntries.push(constructLink(clonedEntry));
+            }
+          }
+        })
+      );
+    }
 
     fields["internalMapping"] = {
-      ["en-US"]: JSON.stringify(newInternalMapping)
+      ["en-US"]: JSON.stringify(internalMapping)
     };
 
     fields["entries"] = {
       ["en-US"]: newEntries
     };
+
+    fields["assets"] = {
+      ["en-US"]: assets
+    };
   }
   return fields;
+};
+
+const createName = (name, rolekey) => {
+  return `${name} ${rolekey}`;
+};
+
+const getClonedEntry = async (space, entryId, name) => {
+  const fetchedEntry = await getEntry(space, entryId);
+
+  const clonedEntry = await cloneEntry(space, fetchedEntry, name);
+
+  return clonedEntry;
 };
 
 const constructLink = entry => {
