174d0ae7abdeb3a80925d42cd9f536d3eab3887e
0xstarcat <xxStarcat@gmail.com>
2020-02-27 14:08:05 -0500

    ..

---
+++ page-component-builder/webpack.config.js
@@ -0,0 +1,50 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/build'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inlineSource: '.(js|css)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/env',
+                {
+                  targets: {
+                    browsers: ['Chrome >=59']
+                  },
+                  modules: false,
+                  loose: true
+                }
+              ],
+              '@babel/react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
